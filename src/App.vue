e1ead31a17bde3fdae5f8ee69234b7104fe897e9
Gal <galrosenthal@gmail.com>
2020-06-24 22:23:15 +0300

    added background

--- src/App.vue
+++ src/App.vue
@@ -42,7 +42,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  /*color: #2c3e50;*/
+  background-image: url("./assets/marble-background.jpg");
   min-height: 100vh;
 }
 
